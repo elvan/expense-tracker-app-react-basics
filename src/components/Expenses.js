833ea91037e266323294eb5a85e41e6d7c14b571
Elvan H <elvanhd@gmail.com>
2021-11-29 08:42:15 +0700

    created expenses component

---
+++ src/components/Expenses.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import ExpenseItem from './ExpenseItem';
+import './Expenses.css';
+
+function Expenses(props) {
+  return (
+    <div className='expenses'>
+      <ExpenseItem
+        key={props.expenses[0].id}
+        title={props.expenses[0].title}
+        amount={props.expenses[0].amount}
+        date={props.expenses[0].date}
+      />
+      <ExpenseItem
+        key={props.expenses[1].id}
+        title={props.expenses[1].title}
+        amount={props.expenses[1].amount}
+        date={props.expenses[1].date}
+      />
+      <ExpenseItem
+        key={props.expenses[2].id}
+        title={props.expenses[2].title}
+        amount={props.expenses[2].amount}
+        date={props.expenses[2].date}
+      />
+      <ExpenseItem
+        key={props.expenses[3].id}
+        title={props.expenses[3].title}
+        amount={props.expenses[3].amount}
+        date={props.expenses[3].date}
+      />
+    </div>
+  );
+}
+
+export default Expenses;
