037f9c6027ec235c52961e18005284f5f9e7f19e
Elvan H <elvanhd@gmail.com>
2021-11-29 08:59:32 +0700

    make use of composition

--- src/components/Expenses.js
+++ src/components/Expenses.js
@@ -1,10 +1,11 @@
 import React from 'react';
+import Card from './Card';
 import ExpenseItem from './ExpenseItem';
 import './Expenses.css';
 
 function Expenses(props) {
   return (
-    <div className='expenses'>
+    <Card className='expenses'>
       <ExpenseItem
         key={props.expenses[0].id}
         title={props.expenses[0].title}
@@ -29,7 +30,7 @@ function Expenses(props) {
         amount={props.expenses[3].amount}
         date={props.expenses[3].date}
       />
-    </div>
+    </Card>
   );
 }
 
